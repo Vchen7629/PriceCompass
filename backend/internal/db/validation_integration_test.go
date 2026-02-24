d474144f1f6f8c366cbb77ad327b8034118c25e6
Vchen7629 <chenvincent7629@gmail.com>
2026-02-23 18:55:09 -0800

    tests(user api): created integration tests for validation db functions

---
+++ backend/internal/db/validation_integration_test.go
@@ -0,0 +1,82 @@
+//go:build integration
+
+package db_test
+
+import (
+	"backend/internal/db"
+	pkgdb "backend/pkg/db"
+	"backend/pkg/test"
+	"context"
+	"testing"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateExistsUserTable(t *testing.T) {
+	pool := testDB.Pool
+	ctx := context.Background()
+
+	t.Run("Should not return an error if the value in column doesnt exist", func(t *testing.T) {
+		test.CleanupTables(t, pool)
+		
+		test.SeedUser(t, pool, "username1", "idk@gmail.com")
+
+		_ = pkgdb.WithTransaction(ctx, pool, func(tx pgx.Tx) error {
+			err := db.ValidateExistsUserTable(ctx, tx, "username", "username10")
+
+			assert.Equal(t, nil, err, "Should not return an error if value doesnt exist")
+
+			return nil
+		})
+	})
+
+	t.Run("Should return an error if the value in column exists", func(t *testing.T) {
+		test.CleanupTables(t, pool)
+		
+		test.SeedUser(t, pool, "username1", "idk@gmail.com")
+
+		_ = pkgdb.WithTransaction(ctx, pool, func(tx pgx.Tx) error {
+			err := db.ValidateExistsUserTable(ctx, tx, "username", "username1")
+
+			assert.NotEqual(t, nil, err, "Should return an error if value exists")
+
+			return nil
+		})
+	})
+}
+
+func TestValidateSession(t *testing.T) {
+	pool := testDB.Pool
+
+	t.Run("Should return an error if nonexistant session token is provided", func(t *testing.T) {
+		test.CleanupTables(t, pool)
+
+		db.InsertNewUser("user1", "idk@gmail.com", "pass123", pool)
+		_, err := db.LoginUser("user1", "pass123", pool)
+
+		assert.Equal(t, nil, err)
+
+		userId, username, err := db.ValidateSession("23udweu", pool)
+
+		assert.NotEqual(t, nil, err, "Should return a non nil error")
+		assert.Equal(t, 0, userId, "validate session returns 0 userId on error")
+		assert.Equal(t, "", username, "returns an empty string on error")
+	})
+
+	t.Run("Should return correct userId and username if correct session token is provided", func(t *testing.T) {
+		test.CleanupTables(t, pool)
+
+		db.InsertNewUser("user1", "idk@gmail.com", "pass123", pool)
+		sessionToken, err := db.LoginUser("user1", "pass123", pool)
+
+		assert.Equal(t, nil, err)
+
+		userId, username, err := db.ValidateSession(sessionToken, pool)
+
+		assert.Equal(t, nil, err, "Should return a nil error")
+		assert.Equal(t, 1, userId)
+		assert.Equal(t, "user1", username)
+	})
+}
+
