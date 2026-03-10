73a92e348b4fdf915f2ca91ebebfd1cef3caa945
Vchen7629 <chenvincent7629@gmail.com>
2026-03-09 19:08:12 -0700

    tests(backend): updated client_test to test properly

--- backend/internal/service/ebay_client_test.go
+++ backend/internal/service/ebay_client_test.go
@@ -17,7 +17,7 @@ func TestNewPlatformClients(t *testing.T) {
 		os.Setenv("EBAY_CLIENT_ID", "")
 		os.Setenv("EBAY_CLIENT_SECRET", "")
 
-		_, err := service.NewPlatformClients()
+		_, err := service.NewEbayOauthClient()
 
 		assert.Equal(t, "missing required env vars: EBAY_CLIENT_ID and/or EBAY_CLIENT_SECRET", err.Error())
 	})
@@ -25,20 +25,9 @@ func TestNewPlatformClients(t *testing.T) {
 	t.Run("throws error on missing best buy env variables", func(t *testing.T) {
 		os.Setenv("EBAY_CLIENT_ID", "hi")
 		os.Setenv("EBAY_CLIENT_SECRET", "hi")
-		os.Setenv("BESTBUY_API_KEY", "")
 
-		_, err := service.NewPlatformClients()
+		_, err := service.NewEbayOauthClient()
 
 		assert.Equal(t, "missing required env variable: BESTBUY_API_KEY", err.Error())
-	})
-
-	t.Run("throws error on missing all env variables", func(t *testing.T) {
-		os.Setenv("EBAY_CLIENT_ID", "")
-		os.Setenv("EBAY_CLIENT_SECRET", "")
-		os.Setenv("BESTBUY_API_KEY", "")
-
-		_, err := service.NewPlatformClients()
-
-		assert.Equal(t, "missing required env vars: EBAY_CLIENT_ID and/or EBAY_CLIENT_SECRET", err.Error())
 	})
 }
